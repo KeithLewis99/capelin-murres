315ac9f0357c2c96ea77877d946dae26a3372c38
A Buren <aburen@mun.ca>
2018-01-02 12:26:06 -0330

    Added Cairns' curves to A Third for The Birds

--- analysis/AThirdForTheBirds.R
+++ analysis/AThirdForTheBirds.R
@@ -35,32 +35,35 @@ athird <- 1/3 * max(cap_biom$nbiomass, na.rm = T)
 
 ## plot ---- 
 #
-type2 <- curve(funcresponse(x, 2.8,.93, 1), 0, 5)
+type2 <- curve(funcresponse(x, 1, .8, 1), 0, 5)
 type2$x <- type2$x - 2
 type3 <- curve(funcresponse(x, 1, 1, 2), 0, 5)
 type3$x <- type3$x - 2
 
-#png('analysis/output/AThirdForTheBirds.png', width = 900, height = 900, bg = 'transparent', pointsize = 15)
+png('analysis/output/AThirdForTheBirds.png', width = 900, height = 900, bg = 'transparent', pointsize = 15)
 # set up the plot
 with(cap_biom, plot(nbiomass, tanh(nbiomass), pch = '', yaxt = 'n', ylab = '', xlab = 'Normalized prey abundance', ylim = c(0,1), xlim = c(-2, 3), xaxp = c(-2,3,5)))
 # plot the rectangle for prior to 1991
 rect(ranreg1$min, 0, ranreg1$max, 1,col = 'grey90', border = 'grey90')
-text(2, .5, 'Observed capelin biomass\n prior to 1991', cex = 0.9)
-# plot the rectangle for after 1990
-rect(ranreg2$min, 0, ranreg2$max, 1,col = 'grey60', border = 'grey60')
-text(-0.23, .4, 'Observed capelin biomass during this study', srt = 90, cex = 0.9)
 # plot the rectangle not observed
 rect(-2, 0, ranreg1$min, 1,col = 'grey50', border = 'grey50')
-text(-1.2, .5, 'Not observed', cex = 0.9)
+# plot the rectangle for after 1990
+rect(ranreg2$min, 0, ranreg2$max, 1,col = 'grey60', border = 'grey60')
 # plot a third for the birds
 abline(v = athird, col = 'red')
-text(athird - 0.1, .4, 'A third of maximum capelin biomass observed', srt = 90, cex = 0.9, col = 'red')
-
 # add a logistic curve
 lines(type2$x, type2$y, lwd = 2)
-curve(logistic(x,a = 3), -2, 3, add = T, lwd = 2)
+curve(logistic(x, a = 3), -2, 3, add = T, lwd = 2)
+# include text
+text(2, .5, 'Observed capelin biomass\n prior to 1991', cex = 0.9)
+text(-0.23, .4, 'Observed capelin biomass during this study', srt = 90, cex = 0.9)
+text(-1.2, .5, 'Not observed', cex = 0.9)
+text(athird - 0.1, .4, 'A third of maximum capelin biomass observed', srt = 90, cex = 0.9, col = 'red')
+
+
 #lines(type3$x, type3$y, lwd = 2)
 
 #lines(cap_biom$nbiomass, funcresponse(cap_biom$nbiomass, 1, 1))
 
-#dev.off()
+dev.off()
+
